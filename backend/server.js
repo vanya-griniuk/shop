36acf5fdd52039f9fc7d083c96f93a5b0520a532
vanya-griniuk <vanyagriniuk@gmail.com>
2022-11-22 17:25:17 +0200

    create database

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 const path = require('path')
 const fs = require('fs')
 const http = require('http');
+const db = require('./db');
 
 const hostname = '127.0.0.1';
 const port = 3001;
 
-const productsPath = path.join(__dirname, './products.json') 
-const products = fs.readFileSync(productsPath, 'utf-8')
+// const productsPath = path.join(__dirname, './products.json') 
+// const products = fs.readFileSync(productsPath, 'utf-8')
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  const { rows: products } = await db.query('SELECT * FROM products');
+
   res.statusCode = 200;
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   res.setHeader('Content-Type', 'application/json');
-  res.end(products);
+  res.end(JSON.stringify(products));
 })
 
 server.listen(port, hostname, () => {
